05634b4d401647690626e7cd97172e3d7a1cce6f
Joaquim <113483260+joaquim-de-melo-neto@users.noreply.github.com>
2023-12-14 16:17:29 -0300

    Update Anagram.java
    refactor: adicionei comentários para explicar o código

--- Anagram.java
+++ Anagram.java
@@ -7,7 +7,7 @@ class Anagram {
     public Anagram(String word) {
         anagram = word;
     }
-
+    // recebe uma palavra e retorna com as letras ordenadas
     public static String sortWord(String string){
         char[] array = string.toUpperCase().toCharArray();
         Arrays.sort(array);
@@ -20,9 +20,10 @@ class Anagram {
 
         String sortedAnagram = sortWord(anagram);
         for(String s: candidates){
+            // verifica se as String candidatas possuem o mesmo tamanho ou são idênticas ao anagrama. Se sim, esse loop é ignorado
             if(anagram.length() != s.length() || anagram.equalsIgnoreCase(s))
                 continue;
-            if(sortedAnagram.equalsIgnoreCase(sortWord(s))) 
+            if(sortedAnagram.equalsIgnoreCase(sortWord(s))) // verifica se são anagramas e em caso positivo adiciona a lista que será retornada
                 list.add(s);
         }
             
