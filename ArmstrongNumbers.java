64b9a8746417f2a9f96ff5db15e3c8c421844e79
Joaquim <113483260+joaquim-de-melo-neto@users.noreply.github.com>
2024-01-29 10:44:59 -0300

    Create ArmstrongNumbers.java

---
+++ ArmstrongNumbers.java
@@ -0,0 +1,19 @@
+class ArmstrongNumbers {
+    static int countDigits(int number){
+        int qtd = 1;
+        while((number /= 10) != 0) qtd++;
+        return qtd;
+    }
+    // verifica se é um número de Armstrong
+    boolean isArmstrongNumber(int number) {
+        if(number == 0) return true;
+        int qtd = countDigits(number);
+        return number == String.valueOf(number)
+            .codePoints() // ou chars()
+            .parallel()
+            .map(i ->(int)Math.pow(Character.getNumericValue(i),qtd))
+            .sum();
+
+    }
+
+}
